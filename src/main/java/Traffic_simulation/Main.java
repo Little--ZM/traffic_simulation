c70f8ccf4a8a4f1c6622925a13a5bc52be5b6107
min.zhu <zhumin1990719@163.com>
2014-03-25 20:41:10 +0800

    初始化项目

---
+++ src/main/java/Traffic_simulation/Main.java
@@ -0,0 +1,17 @@
+package Traffic_simulation;
+
+/**
+ * Created by charleszhu on 14-3-25.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("这是交通仿真");
+
+
+
+    }
+
+
+}
---
+++ src/test/java/Traffic_simulation/MainTest.java
@@ -0,0 +1,13 @@
+package Traffic_simulation;
+
+import org.junit.Test;
+
+/**
+ * Created by charleszhu on 14-3-25.
+ */
+public class MainTest {
+    @Test
+    public void testMain() throws Exception {
+
+    }
+}
